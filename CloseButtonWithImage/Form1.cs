e1feaa88c5c76d0a569841d8f21781b026fd5250
zhenhua Liu <qiyubrother@outlook.com>
2019-08-07 18:14:53 +0800

    添加项目文件。

---
+++ CloseButtonWithImage/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CloseButtonWithImage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNavigation_Paint(object sender, PaintEventArgs e)
+        {
+            var btn = sender as Button;
+            var bmp = Properties.Resources.close_default;
+            e.Graphics.DrawImage(bmp, new Rectangle(btn.Width - 20, 4, 16, 16));
+        }
+
+        private void btnNavigation_MouseClick(object sender, MouseEventArgs e)
+        {
+            var btn = sender as Button;
+            var rect = new Rectangle(btn.Width - 20, 4, 16, 16);
+            if (rect.Contains(e.X, e.Y))
+            {
+                MessageBox.Show("关闭按钮被点击！");
+            }
+        }
+    }
+}
